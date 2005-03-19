da895ef139ba54941eb7dfa90a6a947a54d675ad
afong@org.rmk.(none) <afong@org.rmk.(none)>
2005-03-19 16:22:50 +0000

    [SERIAL] 8250/sbc8560 bug/fix
    Patch from Amy Fong
    
    On the sbc8560 platform, changes were introduced as of 2.6.10-rc2
    which caused the sbc8560 to splutter noise all over the serial
    console.  It seems that ALPHA_KLUDGE_MCR was mulitply and differently
    defined in both drivers/serial/8250.h and include/linux/serialP.h
     
    Below is a patch that clones the definition of ALPHA_KLUDGE_MCR from
    serialP.h, it has been tested and works on the sbc8560.

--- drivers/serial/8250.h
+++ drivers/serial/8250.h
@@ -80,6 +80,13 @@ struct serial8250_config {
  * is cleared, the machine locks up with endless interrupts.
  */
 #define ALPHA_KLUDGE_MCR  (UART_MCR_OUT2 | UART_MCR_OUT1)
+#elif defined(CONFIG_SBC8560)
+/*
+ * WindRiver did something similarly broken on their SBC8560 board. The
+ * UART tristates its IRQ output while OUT2 is clear, but they pulled
+ * the interrupt line _up_ instead of down, so if we register the IRQ
+ * while the UART is in that state, we die in an IRQ storm. */
+#define ALPHA_KLUDGE_MCR (UART_MCR_OUT2)
 #else
 #define ALPHA_KLUDGE_MCR 0
 #endif
