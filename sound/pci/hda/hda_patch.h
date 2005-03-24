315469a004bbd347681accdecb74bf15abc95ff8
Jaroslav Kysela <perex@suse.cz>
2005-03-24 13:54:18 +0100

    [ALSA] Add AD1986A support
    HDA generic driver,HDA Codec driver
    Added the patch for Analog Device AD1986A codec.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/hda_patch.h
+++ sound/pci/hda/hda_patch.h
@@ -6,9 +6,12 @@
 extern struct hda_codec_preset snd_hda_preset_realtek[];
 /* C-Media codecs */
 extern struct hda_codec_preset snd_hda_preset_cmedia[];
+/* Analog Devices codecs */
+extern struct hda_codec_preset snd_hda_preset_analog[];
 
 static const struct hda_codec_preset *hda_preset_tables[] = {
 	snd_hda_preset_realtek,
 	snd_hda_preset_cmedia,
+	snd_hda_preset_analog,
 	NULL
 };
