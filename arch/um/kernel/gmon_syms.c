076652dcdc10891573099e09ec31310b453ae77e
Paolo \'Blaisorblade\' Giarrusso <blaisorblade@yahoo.it>
2005-03-28 03:47:41 -0800

    [PATCH] uml: real fix for __gcov_init symbols
    Correctly export __gcov_init for cases where it's needed, by adding a weak
    definition for the case when GCC does not define this symbol and letting it
    being overriden by the real definition when GCC defines it (recent ones).
    
    Can't be implemented as a test on GCC version because SuSE has a crippled GCC,
    declared as 3.3.4 but having a lot of backported features.
    
    Also, since gcc 3.4.3 requires profiling options even during linking, add
    profiling options to final link stage.
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    CC: Anton Altaparmakov <aia21@cam.ac.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/kernel/gmon_syms.c
+++ arch/um/kernel/gmon_syms.c
@@ -5,14 +5,22 @@
 
 #include "linux/module.h"
 
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ > 3) || \
-	(__GNUC__ == 3 && __GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ >= 4)
-extern void __gcov_init(void *);
-EXPORT_SYMBOL(__gcov_init);
-#else
 extern void __bb_init_func(void *);
 EXPORT_SYMBOL(__bb_init_func);
-#endif
+
+/* This is defined (and referred to in profiling stub code) only by some GCC
+ * versions in libgcov.
+ *
+ * Since SuSE backported the fix, we cannot handle it depending on GCC version.
+ * So, unconditinally export it. But also give it a weak declaration, which will
+ * be overriden by any other one.
+ */
+
+extern void __gcov_init(void *) __attribute__((weak));
+EXPORT_SYMBOL(__gcov_init);
+
+extern void __gcov_merge_add(void *) __attribute__((weak));
+EXPORT_SYMBOL(__gcov_merge_add);
 
 /*
  * Overrides for Emacs so that we follow Linus's tabbing style.
