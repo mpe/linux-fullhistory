7ade05d325f140f76edfb23992abf12001eab063
Roman Zippel <zippel@linux-m68k.org>
2005-03-28 04:08:23 -0800

    [PATCH] hfs: free page buffers in releasepage
    Call try_to_free_buffers() when a page can be released.  This behaviour
    changed during 2.5 and wasn't fixed in hfs while porting it to 2.6.  Also
    remove an unnecessary PageActive() check, only the node ref count is really
    relevant.
    
    Signed-off-by: Roman Zippel <zippel@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/hfs/inode.c
+++ fs/hfs/inode.c
@@ -74,12 +74,6 @@ static int hfs_releasepage(struct page *page, int mask)
 			;
 		else if (atomic_read(&node->refcnt))
 			res = 0;
-		else for (i = 0; i < tree->pages_per_bnode; i++) {
-			if (PageActive(node->page[i])) {
-				res = 0;
-				break;
-			}
-		}
 		if (res && node) {
 			hfs_bnode_unhash(node);
 			hfs_bnode_free(node);
@@ -103,7 +97,7 @@ static int hfs_releasepage(struct page *page, int mask)
 		spin_unlock(&tree->hash_lock);
 	}
 	//printk("releasepage: %lu,%x = %d\n", page->index, mask, res);
-	return res;
+	return res ? try_to_free_buffers(page) : 0;
 }
 
 static int hfs_get_blocks(struct inode *inode, sector_t iblock, unsigned long max_blocks,
