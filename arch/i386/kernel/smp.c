f6fc2736b716229e1df69f6fa9283aad933abeee
Adrian Bunk <bunk@stusta.de>
2005-03-30 16:33:05 -0800

    [PATCH] arch/i386/kernel/smp.c: remove a pointless "inline"
    All callers of send_IPI_mask_sequence are in other files, so marking it 
    "inline" is quite pointless.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/smp.c
+++ arch/i386/kernel/smp.c
@@ -188,7 +188,7 @@ void send_IPI_mask_bitmask(cpumask_t cpumask, int vector)
 	local_irq_restore(flags);
 }
 
-inline void send_IPI_mask_sequence(cpumask_t mask, int vector)
+void send_IPI_mask_sequence(cpumask_t mask, int vector)
 {
 	unsigned long cfg, flags;
 	unsigned int query_cpu;
